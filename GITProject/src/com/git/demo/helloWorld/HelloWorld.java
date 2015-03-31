0c5436bc3bdb63ab06417a0fdaabf33e3a4b837e
x209215 <x209215@SY4-65M5CY1.LUV.AD.SWACORP.com>
2015-03-31 14:39:56 -0500

    Test project

---
+++ GITProject/src/com/git/demo/helloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.git.demo.helloWorld;
+
+public class HelloWorld {
+	public static void main(String[] args){
+		
+	displayGreetings();
+	
+	}
+
+	private static void displayGreetings() {
+		System.out.println("Welcome to HelloWorld Project");
+		
+	}
+
+}
